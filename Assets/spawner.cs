97acc32bd822b906aa58bd2ca5a0b622cee39b43
RasmusEngstroem <rengst12@student.aau.dk>
2014-03-04 11:52:06 +0100

    Just a folder

---
+++ Assets/spawner.cs
@@ -0,0 +1,27 @@
+﻿	using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+	
+	public Transform Spawn;
+	public float spawnTime = 0.1f;
+	
+	private int spawned = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > spawned * spawnTime){
+			spawned++;
+			Instantiate(Spawn, transform.position + transform.forward, Quaternion.identity);
+		   
+		}
+	
+	}
+	
+	
+}
