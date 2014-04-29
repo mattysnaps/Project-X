34332fa6dfb80f07be2c9d4ee6a54934412339c5
mattysnaps <mfryd12@student.aau.dk>
2014-04-29 17:01:25 +0200

    no message

---
+++ OOSE project/New Unity Project/Assets/Scripts/BackButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	 				
+	void OnGUI()
+	{
+                       
+		if (GUI.Button(new Rect(Screen.width/2+500, 15, 150, 100),"BACK TO MAIN MENU"))
+		{
+		Application.LoadLevel(0);
+		}                  
+			
+	}	
+}
+
