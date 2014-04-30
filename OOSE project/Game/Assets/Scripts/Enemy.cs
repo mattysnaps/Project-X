ce0df7666a180e5ca625ea6d845f48fe7950adc1
C-Roland <c.roland999@live.dk>
2014-04-30 12:18:22 +0200

    Made some minor changes for the player healthbar, so that it display better on different resolution.

--- OOSE project/Game/Assets/Scripts/Enemy.cs
+++ OOSE project/Game/Assets/Scripts/Enemy.cs
@@ -5,14 +5,14 @@ public class Enemy : MonoBehaviour {
 
 	// Health values
 	public float maxHealth = 100;
-	public float curHealth = 50;
+	public float curHealth = 100;
 
 	// Healthbar size
 	public float healthBarLength;
 
 	void OnGUI(){ //Makes a GUI box, used for helthbar
 		GUI.color = Color.red; // Health bar text color is red
-		GUI.Box(new Rect(860, 380, healthBarLength/10, 20), curHealth + "/" + maxHealth + " HP"); // the dimentions of the healthbar and the infomation it contains
+		GUI.Box(new Rect(10, 10, healthBarLength, 20), curHealth + "/" + maxHealth + " HP"); // the dimentions of the healthbar and the infomation it contains
 
 		}
 
